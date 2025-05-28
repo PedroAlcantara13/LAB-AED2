eb5b660945bae619b625fb2a0e81fdfcd4afe1bb
Pedro Alcantara <alcantarap37@gmail.com>
2025-05-28 15:06:04 -0300

    Iniciando e Q1

--- q1/q1.c
+++ q1/q1.c
@@ -1,5 +1,35 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
 
-int main(){
-    printf("Hello World");
+bool ehPrimo(int n) {
+    if (n <= 1) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+
+    for (int i = 3; i <= sqrt(n); i += 2) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int inicio, fim;
+
+    printf("Digite o valor inicial do intervalo: ");
+    scanf("%d", &inicio);
+
+    printf("Digite o valor final do intervalo: ");
+    scanf("%d", &fim);
+
+    printf("Numeros primos entre %d e %d:\n", inicio, fim);
+    for (int i = inicio; i <= fim; i++) {
+        if (ehPrimo(i)) {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+    return 0;
 }
